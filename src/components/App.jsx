8b400ce441058ed421f67cf2a208b0b6feab60c7
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2022-12-11 20:55:34 +0100

    Fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,25 +5,15 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
 export function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  );
   const [filter, setFilter] = useState('');
-
-   useEffect(() => {
-    const contactsStorage = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contactsStorage);
-
-    if (parsedContacts) {
-      setContacts(parsedContacts);
-    } else {
-      return;
-    }
-  }, []);
-
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
@@ -33,6 +23,7 @@ export function App() {
     const findName = contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
+    
     if (findName) {
       return alert(`${name} is already in contacts.`);
     }
